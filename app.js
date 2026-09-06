15e8b8c489346170ff83b98f30087d8294cb0ee9
shoutyinc <shoutyinc@icloud.com>
2026-09-05 23:49:10 -0500

    Remove import statement for THREE.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
+const world=document.getElementById("world");
 
 const world=document.getElementById("world");
 
